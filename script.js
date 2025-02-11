7cc742becb2e920aeb53729bb9c13a31e8484173
manikanik <isaacnewton7883@gmail.com>
2025-02-12 00:42:13 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,105 +1,105 @@
-const calculator = {
-    displayValue: '0',
-    firstOperand: null,
-    waitingForSecondOperand: false,
-    operator: null,
-};
+// const calculator = {
+//     displayValue: '0',
+//     firstOperand: null,
+//     waitingForSecondOperand: false,
+//     operator: null,
+// };
 
-function inputDigit(digit) {
-    const { displayValue, waitingForSecondOperand } = calculator;
+// function inputDigit(digit) {
+//     const { displayValue, waitingForSecondOperand } = calculator;
 
-    if (waitingForSecondOperand === true) {
-        calculator.displayValue = digit;
-        calculator.waitingForSecondOperand = false;
-    } else {
-        calculator.displayValue = displayValue === '0' ? digit : displayValue + digit;
-    }
-}
+//     if (waitingForSecondOperand === true) {
+//         calculator.displayValue = digit;
+//         calculator.waitingForSecondOperand = false;
+//     } else {
+//         calculator.displayValue = displayValue === '0' ? digit : displayValue + digit;
+//     }
+// }
 
-function inputDecimal(dot) {
-    if (calculator.waitingForSecondOperand === true) return;
+// function inputDecimal(dot) {
+//     if (calculator.waitingForSecondOperand === true) return;
 
-    if (!calculator.displayValue.includes(dot)) {
-        calculator.displayValue += dot;
-    }
-}
+//     if (!calculator.displayValue.includes(dot)) {
+//         calculator.displayValue += dot;
+//     }
+// }
 
-function handleOperator(nextOperator) {
-    const { firstOperand, displayValue, operator } = calculator;
-    const inputValue = parseFloat(displayValue);
+// function handleOperator(nextOperator) {
+//     const { firstOperand, displayValue, operator } = calculator;
+//     const inputValue = parseFloat(displayValue);
 
-    if (operator && calculator.waitingForSecondOperand) {
-        calculator.operator = nextOperator;
-        return;
-    }
+//     if (operator && calculator.waitingForSecondOperand) {
+//         calculator.operator = nextOperator;
+//         return;
+//     }
 
-    if (firstOperand == null && !isNaN(inputValue)) {
-        calculator.firstOperand = inputValue;
-    } else if (operator) {
-        const result = calculate(firstOperand, inputValue, operator);
+//     if (firstOperand == null && !isNaN(inputValue)) {
+//         calculator.firstOperand = inputValue;
+//     } else if (operator) {
+//         const result = calculate(firstOperand, inputValue, operator);
 
-        calculator.displayValue = `${parseFloat(result.toFixed(7))}`;
-        calculator.firstOperand = result;
-    }
+//         calculator.displayValue = `${parseFloat(result.toFixed(7))}`;
+//         calculator.firstOperand = result;
+//     }
 
-    calculator.waitingForSecondOperand = true;
-    calculator.operator = nextOperator;
-}
+//     calculator.waitingForSecondOperand = true;
+//     calculator.operator = nextOperator;
+// }
 
-function calculate(firstOperand, secondOperand, operator) {
-    if (operator === '+') {
-        return firstOperand + secondOperand;
-    } else if (operator === '-') {
-        return firstOperand - secondOperand;
-    } else if (operator === '*') {
-        return firstOperand * secondOperand;
-    } else if (operator === '/') {
-        return firstOperand / secondOperand;
-    }
+// function calculate(firstOperand, secondOperand, operator) {
+//     if (operator === '+') {
+//         return firstOperand + secondOperand;
+//     } else if (operator === '-') {
+//         return firstOperand - secondOperand;
+//     } else if (operator === '*') {
+//         return firstOperand * secondOperand;
+//     } else if (operator === '/') {
+//         return firstOperand / secondOperand;
+//     }
 
-    return secondOperand;
-}
+//     return secondOperand;
+// }
 
-function resetCalculator() {
-    calculator.displayValue = '0';
-    calculator.firstOperand = null;
-    calculator.waitingForSecondOperand = false;
-    calculator.operator = null;
-}
+// function resetCalculator() {
+//     calculator.displayValue = '0';
+//     calculator.firstOperand = null;
+//     calculator.waitingForSecondOperand = false;
+//     calculator.operator = null;
+// }
 
-function updateDisplay() {
-    const display = document.querySelector('.calculator-screen');
-    display.value = calculator.displayValue;
-}
+// function updateDisplay() {
+//     const display = document.querySelector('.calculator-screen');
+//     display.value = calculator.displayValue;
+// }
 
-const keys = document.querySelector('.calculator-keys');
-keys.addEventListener('click', (event) => {
-    const { target } = event;
-    const { value } = target;
+// const keys = document.querySelector('.calculator-keys');
+// keys.addEventListener('click', (event) => {
+//     const { target } = event;
+//     const { value } = target;
 
-    if (!target.matches('button')) {
-        return;
-    }
+//     if (!target.matches('button')) {
+//         return;
+//     }
 
-    switch (value) {
-        case '+':
-        case '-':
-        case '*':
-        case '/':
-        case '=':
-            handleOperator(value);
-            break;
-        case '.':
-            inputDecimal(value);
-            break;
-        case 'all-clear':
-            resetCalculator();
-            break;
-        default:
-            if (Number.isInteger(parseFloat(value))) {
-                inputDigit(value);
-            }
-    }
+//     switch (value) {
+//         case '+':
+//         case '-':
+//         case '*':
+//         case '/':
+//         case '=':
+//             handleOperator(value);
+//             break;
+//         case '.':
+//             inputDecimal(value);
+//             break;
+//         case 'all-clear':
+//             resetCalculator();
+//             break;
+//         default:
+//             if (Number.isInteger(parseFloat(value))) {
+//                 inputDigit(value);
+//             }
+//     }
 
-    updateDisplay();
-});
+//     updateDisplay();
+// });
